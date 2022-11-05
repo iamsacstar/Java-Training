1fe7198e288d7eec42afb82217d37517dd0d6c29
Sachin Kumar <110603659+iamsacstar@users.noreply.github.com>
2022-11-05 11:44:14 +0530

    Add files via upload

---
+++ 7. Vowel.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Vowel{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a character to check: ");
+		char ch = sc.next().charAt(0);
+		if(ch =='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		System.out.println("The entered character is a vowel.");
+		else
+		System.out.println("The entered character is not a vowel, it is a constant.");
+	}	
+}
